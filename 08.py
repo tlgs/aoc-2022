5dfc16084553428a23406a6d3ec94f0b315ea94e
Tiago Seabra <tlgs@users.noreply.github.com>
2022-12-08 20:12:31 +0000

    Improve day 08 part 2 performance by using spiraled traversal
    This again improves a bit of performance: 75ms -> 65ms.
    It's a bit hacky, but works by pre-computing the best theoretical
    maximum scores on every grid position and traversing them in descending
    order. **This allows for an early stopping condition.**

--- 08.py
+++ 08.py
@@ -1,56 +1,78 @@
 import sys
-from itertools import product, takewhile
+from itertools import count, cycle
 
 
 def parse_input(puzzle_input):
-    grid = [[int(x) for x in line] for line in puzzle_input.splitlines()]
-    return (grid,)
+    lines = puzzle_input.splitlines()
+    grid = {(x, y): int(v) for y, line in enumerate(lines) for x, v in enumerate(line)}
+    return (grid, len(lines))
 
 
-def part_one(grid):
-    n_rows, n_cols = len(grid), len(grid[0])
-
+def part_one(grid, n):
     visible = set()
 
     # up / down
-    for x in range(n_cols):
-        for traversal in [range(n_rows), range(n_rows - 1, -1, -1)]:
+    for x in range(n):
+        for traversal in [range(n), range(n - 1, -1, -1)]:
             v = -1
             for y in traversal:
-                if grid[y][x] > v:
+                if grid[x, y] > v:
                     visible.add((x, y))
-                    v = grid[y][x]
+                    v = grid[x, y]
 
     # left / right
-    for y in range(n_rows):
-        for traversal in [range(n_cols), range(n_cols - 1, -1, -1)]:
+    for y in range(n):
+        for traversal in [range(n), range(n - 1, -1, -1)]:
             v = -1
             for x in traversal:
-                if grid[y][x] > v:
+                if grid[x, y] > v:
                     visible.add((x, y))
-                    v = grid[y][x]
+                    v = grid[x, y]
 
     return len(visible)
 
 
-def distance(height, trees):
-    n = sum(1 for _ in takewhile(lambda x: x < height, trees))
-    return n + 1
+def spiral(x=0, y=0):
+    directions = cycle([(-1, 0), (0, -1), (1, 0), (0, 1)])
+    yield x, y
+
+    for i in count(1):
+        for _ in range(2):
+            dx, dy = next(directions)
+            for _ in range(i):
+                x, y = x + dx, y + dy
+                yield x, y
 
 
-def part_two(grid):
-    n_rows, n_cols = len(grid), len(grid[0])
+def countwhile(predicate, iterable):
+    count = 0
+    for x in iterable:
+        if predicate(x):
+            count += 1
+        else:
+            break
+
+    return count
+
+
+def part_two(grid, n):
+    maximums = {
+        (x, y): y * (n - 1 - y) * x * (n - 1 - x) for y in range(n) for x in range(n)
+    }
 
     best = 0
-    for (x, y) in product(range(1, n_cols - 1), range(1, n_rows - 1)):
-        h = grid[y][x]
+    for x, y in spiral(n // 2, n // 2):
+        if best > maximums[x, y]:
+            break
 
-        up = distance(h, (grid[my][x] for my in range(y - 1, 0, -1)))
-        down = distance(h, (grid[my][x] for my in range(y + 1, n_rows - 1)))
-        left = distance(h, (grid[y][mx] for mx in range(x - 1, 0, -1)))
-        right = distance(h, (grid[y][mx] for mx in range(x + 1, n_cols - 1)))
+        h = grid[x, y]
+        up___ = countwhile(lambda v: v < h, (grid[x, yy] for yy in range(y - 1, 0, -1)))
+        down_ = countwhile(lambda v: v < h, (grid[x, yy] for yy in range(y + 1, n - 1)))
+        left_ = countwhile(lambda v: v < h, (grid[xx, y] for xx in range(x - 1, 0, -1)))
+        right = countwhile(lambda v: v < h, (grid[xx, y] for xx in range(x + 1, n - 1)))
 
-        best = max(best, up * down * left * right)
+        score = (1 + up___) * (1 + down_) * (1 + left_) * (1 + right)
+        best = score if score > best else best
 
     return best
 
