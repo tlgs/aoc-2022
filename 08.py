12c27c428eed9952d7b439ae4ac93690e3c23178
Tiago Seabra <tlgs@users.noreply.github.com>
2022-12-08 21:19:35 +0000

    Improve day 08 part 2 performance by using monotonic stacks
    Another performance change, this time with stacks: 65ms -> 56ms.
    Shoutout to /u/Lucews for pointing out this approach on Reddit.

--- 08.py
+++ 08.py
@@ -1,5 +1,5 @@
 import sys
-from itertools import count, cycle
+from collections import defaultdict
 
 
 def parse_input(puzzle_input):
@@ -32,49 +32,42 @@ def part_one(grid, n):
     return len(visible)
 
 
-def spiral(x=0, y=0):
-    directions = cycle([(-1, 0), (0, -1), (1, 0), (0, 1)])
-    yield x, y
-
-    for i in count(1):
-        for _ in range(2):
-            dx, dy = next(directions)
-            for _ in range(i):
-                x, y = x + dx, y + dy
-                yield x, y
-
-
-def countwhile(predicate, iterable):
-    count = 0
-    for x in iterable:
-        if predicate(x):
-            count += 1
-        else:
-            break
-
-    return count
-
-
 def part_two(grid, n):
-    maximums = {
-        (x, y): y * (n - 1 - y) * x * (n - 1 - x) for y in range(n) for x in range(n)
-    }
+    scores = defaultdict(lambda: 1)
 
-    best = 0
-    for x, y in spiral(n // 2, n // 2):
-        if best > maximums[x, y]:
-            break
+    # up / down
+    for x in range(n):
+        stack_a, stack_b = [], []
+        for ya, yb in zip(range(n), range(n - 1, -1, -1)):
+            while stack_a and stack_a[-1][1] < grid[x, ya]:
+                stack_a.pop()
 
-        h = grid[x, y]
-        up___ = countwhile(lambda v: v < h, (grid[x, yy] for yy in range(y - 1, 0, -1)))
-        down_ = countwhile(lambda v: v < h, (grid[x, yy] for yy in range(y + 1, n - 1)))
-        left_ = countwhile(lambda v: v < h, (grid[xx, y] for xx in range(x - 1, 0, -1)))
-        right = countwhile(lambda v: v < h, (grid[xx, y] for xx in range(x + 1, n - 1)))
+            while stack_b and stack_b[-1][1] < grid[x, yb]:
+                stack_b.pop()
 
-        score = (1 + up___) * (1 + down_) * (1 + left_) * (1 + right)
-        best = score if score > best else best
+            scores[x, ya] *= (ya - stack_a[-1][0]) if stack_a else ya
+            scores[x, yb] *= (stack_b[-1][0] - yb) if stack_b else n - 1 - yb
 
-    return best
+            stack_a.append((ya, grid[x, ya]))
+            stack_b.append((yb, grid[x, yb]))
+
+    # left / right
+    for y in range(n):
+        stack_a, stack_b = [], []
+        for xa, xb in zip(range(n), range(n - 1, -1, -1)):
+            while stack_a and stack_a[-1][1] < grid[xa, y]:
+                stack_a.pop()
+
+            while stack_b and stack_b[-1][1] < grid[xb, y]:
+                stack_b.pop()
+
+            scores[xa, y] *= (xa - stack_a[-1][0]) if stack_a else xa
+            scores[xb, y] *= (stack_b[-1][0] - xb) if stack_b else n - 1 - xb
+
+            stack_a.append((xa, grid[xa, y]))
+            stack_b.append((xb, grid[xb, y]))
+
+    return max(scores.values())
 
 
 class Test:
